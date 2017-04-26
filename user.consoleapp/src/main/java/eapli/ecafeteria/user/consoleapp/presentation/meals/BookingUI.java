6f7c8c86cff84c33ee4b5883b83dbca77495ac94
1970581 <1970581@isep.ipp.pt>
2017-04-26 23:43:52 +0100

    SRR-06 MealBooking SD da parte b, issue #10 Inclusao de codigo comentao na classe BookingUI relativa a mostar informacao da Meal indicada. SD actualizado, (corrigidas setas de resposta).  G3 Hugo & Pedro Pereira

--- user.consoleapp/src/main/java/eapli/ecafeteria/user/consoleapp/presentation/meals/BookingUI.java
+++ user.consoleapp/src/main/java/eapli/ecafeteria/user/consoleapp/presentation/meals/BookingUI.java
@@ -5,7 +5,12 @@
  */
 package eapli.ecafeteria.user.consoleapp.presentation.meals;
 
+import eapli.ecafeteria.application.booking.ShowMealInfoBookingController;
 import eapli.framework.presentation.console.AbstractUI;
+import eapli.ecafeteria.domain.meals.Meal;
+import eapli.ecafeteria.application.booking.ShowMealInfoBookingController;
+import eapli.ecafeteria.domain.meals.NutricionalInfo;
+
 
 /**
  * SRR-06 Classe do UI de escolha e booking de uma unica refeicao SRR-06
@@ -16,12 +21,42 @@ public class BookingUI extends AbstractUI{
     @Override
     protected boolean doShow() {
         
-        System.out.println("Entramos no BookingUi");
-        
         //INSERIR codigo do bookingUI aqui.
         
         
-        System.out.println("Terminamos o BookingUi");
+        //Codigo da parte de mostrar info da Meal indicada pelo user. Hugo & Pedro        
+        Meal mealSelecionada = null;  //meal que o utilizador indicou no codigo acima.
+        {
+        /*
+            //Instranciacao do controller.
+            ShowMealInfoBookingController showController = new ShowMealInfoBookingController();
+        
+            //Mostrar as cal+sal da meal:
+            NutricionalInfo informacaoNutricionalDaMealSelecionada = showController.obtainNutricionalInfo(mealSelecionada);
+            System.out.println("Meal Nutritional info: ");
+            NutricionalInfoPrinter nutricionalInfoPrinter = new NutricionalInfoPrinter();
+            nutricionalInfoPrinter.visit(informacaoNutricionalDaMealSelecionada);
+        
+            //Mostrar os alegenicos:
+            System.out.println("Allergen present in the meal:");
+            //List<DishAllergen> listaDishAlergenicos = showController.obtainListAllergen(Meal meal);
+            DishAllergenPrinter dishAllergenPrinter = new DishAllergenPrinter();
+            for(DishAllergen dishAllergen : listaDishAllergenicos){
+                dishAllergenPrinter.visit(dishAllergen);
+            }
+            
+            //Mostrar warning caso allergico
+            if(showController.isAllergic(mealSelecionada)) System.out.println("WARNING you are allergic to some meal contents!!!");
+            
+            //Mostrar consumo de calorias/sal dos bookings desta semana incluindo a mealSelecionada.
+            NutritionalInfo weekCommulativeNutritional = showController.returnWeakInfoMeal(meal)
+            nutricionalInfoPrinter.visit( weekCommulativeNutritional );
+        */
+        }
+        //Fim da parte de mostrar info. Hugo & Pedro
+        
+        
+        
         
         return false;   //Nao identifiquei uso, pelo consegui constatar. Alguns retornam falso, outros true. O return true if this "scope" should end(HugoB)
     }
