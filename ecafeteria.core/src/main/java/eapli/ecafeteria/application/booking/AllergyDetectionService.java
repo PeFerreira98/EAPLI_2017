89025504b9bd5d7482a3af02b05b0051576e0689
1970581 <1970581@isep.ipp.pt>
2017-05-01 18:24:07 +0100

    SRR-06B MealBooking issue #10 G3 : AllergyDetectionService implementacao parcial. AllergyDetectionService, bloqueado devido a falta do codigo de suporte relativo a DishAllergen e AllergenProfile de outros grupos.

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/booking/AllergyDetectionService.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/booking/AllergyDetectionService.java
@@ -6,11 +6,16 @@
 package eapli.ecafeteria.application.booking;
 
 import eapli.ecafeteria.domain.cafeteria.CafeteriaUser;
+import eapli.ecafeteria.domain.meals.Allergen;
+import eapli.ecafeteria.domain.meals.Dish;
 import eapli.ecafeteria.domain.meals.Meal;
+import eapli.ecafeteria.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 
 /**
- * Servico cuja responsabilidade e detectar alergias de users com Meals. OBSERVABLE
+ * Servico cuja responsabilidade e detectar allergenicos e alergias de users e Meals. OBSERVABLE
  * @author Pedro Pereira
  */
 public class AllergyDetectionService extends Observable{
@@ -26,10 +31,9 @@ public class AllergyDetectionService extends Observable{
         boolean wasAllergyDetected = false;
 
         
-//        // TODO
-//        //List<DishAllergen> dishAllergenList = dish.dishAllergens();
-        System.out.println("FALTA IMPLEMENTAR ALLERGYDETECTIONSERVICE");
+        List <Allergen> listaDeAllegenicosComuns = this.listCommonAllergen(cafeteriaUser, meal);
         
+        if (! listaDeAllegenicosComuns.isEmpty()) wasAllergyDetected = true;    //Se a lista nao esta vazia entao vai existir alergia.
         
         if(wasAllergyDetected){             //Se detectamos uma alergia,
             this.setChanged();              // Vamos informar que estamos alterados
@@ -38,6 +42,68 @@ public class AllergyDetectionService extends Observable{
         
         return wasAllergyDetected;
     }
+    
+    /**
+     * Metodo que devolve todos os alergenicos existentes numa Meal.
+     * @param meal meal a obter os alergenicos
+     * @return List de Allergen da meal
+     */
+    public List<Allergen> listAllergen(Meal meal){
+        List<Allergen> listaAllergenicos = new ArrayList();
+        if(meal == null) return listaAllergenicos;
+        
+        Dish dish = meal.dish();
+        
+        //TODO
+        // Obter uma lista com todos os DishAllergen do Dish.
+        // Ir a cada DishAllerger, obter o Allergen e addicionar a lista.
+        System.out.println("FALTA IMPLEMENTAR listAllergen(Meal meal) Hugo & Pedro");
+        
+        return listaAllergenicos;
+    }
+    
+    
+    /**
+     * Metodo que devolve todos os alergenicos associados a um CafeteriaUser
+     * @param cafeteriaUser cafeteriaUser do qual obter os alergenicos
+     * @return List de Allergen associados ao cafeteriaUser
+     */
+    public List<Allergen> listAllergen(CafeteriaUser cafeteriaUser){
+        List<Allergen> listaAllergenicos = new ArrayList();
+        if (cafeteriaUser == null)  return listaAllergenicos;
+        
+        //TODO
+        // Obter uma lista com todos os AllergenProfile do CafeteriaUser
+        // Obter os Allergen do AllergenProfiles e adicionar a lista.         
+        System.out.println("FALTA IMPLEMENTAR listAllergen(CafeteriaUser cafeteriaUser) Hugo & Pedro");
+        
+        return listaAllergenicos;
+    }
+    
+    /**
+     * Devolve uma lista com os allergenicos em commum entre um user e uma meal.
+     * @param cafeteriaUser o cafeteria User
+     * @param meal a meal em questao
+     * @return a lista de allergenicos presentes na meal e no perfil do cafeteriaUser
+     */
+    public List<Allergen> listCommonAllergen(CafeteriaUser cafeteriaUser, Meal meal){
+        
+        List <Allergen> listaUser = this.listAllergen(cafeteriaUser);
+        List <Allergen> listaMeal = this.listAllergen(meal);
+        List <Allergen> listaCommon = new ArrayList();
+        
+        //Comparamos todos os alergenicos de uma lista com o de outra 
+        // e se o allergenico for o mesmo e nao estiver na lista, adicionamos.
+        for(Allergen allergenUser : listaUser){
+            for(Allergen allergenMeal : listaMeal){
+                if(allergenUser.equals(allergenMeal) && !listaCommon.contains(allergenUser)) listaCommon.add(allergenUser);
+            }
+        }
+        
+        return listaCommon;
+    }
+    
+    
     
     
 }
