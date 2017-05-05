e45ce38e973ee9fb04e8a350b3054eee8c1f9a5b
Pedro Pereira <1130626@isep.ipp.pt>
2017-05-05 16:20:57 +0100

    Fixing ECafeteriaUserApp

--- user.consoleapp/src/main/java/eapli/ecafeteria/user/consoleapp/ECafeteriaUserApp.java
+++ user.consoleapp/src/main/java/eapli/ecafeteria/user/consoleapp/ECafeteriaUserApp.java
@@ -21,26 +21,3 @@ public final class ECafeteriaUserApp {
 	new FrontMenu().show();
     }
 }
-package eapli.ecafeteria.user.consoleapp;
-
-import eapli.ecafeteria.user.consoleapp.presentation.FrontMenu;
-
-/**
- * eCafeteria User App.
- */
-public final class ECafeteriaUserApp {
-
-    /**
-     * Empty constructor is private to avoid instantiation of this class.
-     */
-    private ECafeteriaUserApp() {
-    }
-
-    public static void main(final String[] args) {
-
-        // only needed because of the in memory persistence
-//        new ECafeteriaBootstrap().execute();
-
-        new FrontMenu().show();
-    }
-}
