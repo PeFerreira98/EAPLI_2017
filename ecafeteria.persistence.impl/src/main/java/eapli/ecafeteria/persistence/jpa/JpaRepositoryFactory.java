1736b2f27ea711c214f131bbd175a662034783b9
Rafael Vieira <1141233@isep.ipp.pt>
2017-05-05 16:22:47 +0100

    fix JpaRepositoryFactory

--- ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaRepositoryFactory.java
+++ ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaRepositoryFactory.java
@@ -1,9 +1,17 @@
 package eapli.ecafeteria.persistence.jpa;
 
 import eapli.ecafeteria.Application;
+import eapli.ecafeteria.persistence.AllergenRepository;
+import eapli.ecafeteria.persistence.BookingRepository;
+import eapli.ecafeteria.persistence.DishAllergenRepository;
 import eapli.ecafeteria.persistence.DishRepository;
 import eapli.ecafeteria.persistence.DishTypeRepository;
 import eapli.ecafeteria.persistence.MaterialRepository;
+import eapli.ecafeteria.persistence.MealRepository;
+import eapli.ecafeteria.persistence.MealTypeRepository;
+import eapli.ecafeteria.persistence.MenuRepository;
+import eapli.ecafeteria.persistence.NutricionalProfileAllergenRepository;
+import eapli.ecafeteria.persistence.NutricionalProfileRepository;
 import eapli.ecafeteria.persistence.OrganicUnitRepository;
 import eapli.ecafeteria.persistence.RepositoryFactory;
 import eapli.ecafeteria.persistence.SignupRequestRepository;
@@ -19,41 +27,81 @@ public class JpaRepositoryFactory implements RepositoryFactory {
 
     @Override
     public UserRepository users(TransactionalContext autoTx) {
-	return new JpaUserRepository(autoTx);
+        return new JpaUserRepository(autoTx);
     }
 
     @Override
     public DishTypeRepository dishTypes() {
-	return new JpaDishTypeRepository();
+        return new JpaDishTypeRepository();
     }
 
     @Override
     public OrganicUnitRepository organicUnits() {
-	return new JpaOrganicUnitRepository();
+        return new JpaOrganicUnitRepository();
     }
 
     @Override
     public JpaCafeteriaUserRepository cafeteriaUsers(TransactionalContext autoTx) {
-	return new JpaCafeteriaUserRepository(autoTx);
+        return new JpaCafeteriaUserRepository(autoTx);
     }
 
     @Override
     public SignupRequestRepository signupRequests(TransactionalContext autoTx) {
-	return new JpaSignupRequestRepository(autoTx);
+        return new JpaSignupRequestRepository(autoTx);
     }
 
     @Override
     public DishRepository dishes() {
-	return new JpaDishRepository();
+        return new JpaDishRepository();
     }
 
     @Override
     public MaterialRepository materials() {
-	return new JpaMaterialRepository();
+        return new JpaMaterialRepository();
     }
 
     @Override
     public TransactionalContext buildTransactionalContext() {
-	return new JpaTransactionalContext(Application.settings().getPersistenceUnitName());
+        return new JpaTransactionalContext(Application.settings().getPersistenceUnitName());
+    }
+
+    @Override
+    public MenuRepository menus() {
+        return new JpaMenuRepository();
+    }
+
+    @Override
+    public MealTypeRepository mealTypes() {
+        return new JpaMealTypeRepository();
+    }
+
+    @Override
+    public MealRepository meals() {
+        return new JpaMealRepository();
+    }
+
+    @Override
+    public AllergenRepository allergens() {
+        return new JpaAllergenRepository();
+    }
+
+    @Override
+    public DishAllergenRepository dishAllergens() {
+        return new JpaDishAllergenRepository();
+    }
+
+    @Override
+    public BookingRepository reserves() {
+        return new JpaBookingRepository();
+    }
+
+    @Override
+    public NutricionalProfileRepository nutricionalProfiles() {
+        return new JpaNutricionalProfileRepository();
+    }
+
+    @Override
+    public NutricionalProfileAllergenRepository nutricionalProfileAllergens() {
+        return new JpaNutricionalProfileAllergenRepository();
     }
 }
