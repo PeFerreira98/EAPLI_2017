6af26cab1a912662c50dc816c3bf968c2a60cbf2
Rafael Vieira <1141233@isep.ipp.pt>
2017-05-24 20:54:28 +0100

    ADMIN-08 - Alertas de cozinha repositorio

---
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/persistence/BookingAlertRepository.java
@@ -0,0 +1,11 @@
+package eapli.ecafeteria.persistence;
+
+import eapli.ecafeteria.domain.alert.BookingAlert;
+import eapli.framework.persistence.repositories.DataRepository;
+
+/**
+ *
+ * @author Rafael Vieira <1141233@isep.ipp.pt>
+ */
+public interface BookingAlertRepository extends DataRepository<BookingAlert, Long>{
+}
