acfe9d6c9a2be4e06c5490a889453d5877dd5014
Tiago Silvestre <1140948@isep.ipp.pt>
2017-05-25 14:15:05 +0100

    Botão de acesso ao Nutricional Profile ao user. Comentado por problemas de dependências

--- consoleapp.common/src/main/java/eapli/cafeteria/consoleapp/presentation/MyUserMenu.java
+++ consoleapp.common/src/main/java/eapli/cafeteria/consoleapp/presentation/MyUserMenu.java
@@ -13,6 +13,7 @@ public class MyUserMenu extends Menu {
 
     // MY USER
     private static final int CHANGE_PASSWORD_OPTION = 1;
+//    private static final int NUTRICIONAL_PROFILE_OPTION = 2;
     private static final int LOGIN_OPTION = 2;
     private static final int LOGOUT_OPTION = 3;
 
@@ -23,6 +24,7 @@ public class MyUserMenu extends Menu {
 
     private void buildMyUserMenu() {
         add(new MenuItem(CHANGE_PASSWORD_OPTION, "Change password", new ShowMessageAction("Not implemented yet")));
+//        add(new MenuItem(NUTRICIONAL_PROFILE_OPTION, "Register Nutricional Profile", new RegisterNutricionalProfileAction()));
         add(new MenuItem(LOGIN_OPTION, "Change user (Login)", new LoginAction()));
         add(new MenuItem(LOGOUT_OPTION, "Logout", new LogoutAction()));
         add(new MenuItem(EXIT_OPTION, "Return ", new ReturnAction()));
